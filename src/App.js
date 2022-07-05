13683fd75723fc574106aa725dbeb35142f1d10e
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-05 17:38:22 +0300

    UI basic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit
+//           {' '}
+//           <code>src/App.js</code>
+//           {' '}
+//           and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const Numbers = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i += 1) {
+      digits.push(
+        <button type="button" key={i}>{i}</button>,
+      );
+    }
+    return digits;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="result">
+          0
+        </div>
+        <div className="operator">
+          <button type="button">/</button>
+          <button type="button">*</button>
+          <button type="button">-</button>
+          <button type="button">+</button>
+        </div>
+        <div className="digits">
+          { Numbers() }
+          <button type="button">0</button>
+          <button type="button">.</button>
+          <button type="button">=</button>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
