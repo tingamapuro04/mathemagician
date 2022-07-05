48ac0e63c49490e9b92fd1042dc7c6cc8089ecf0
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-06 00:37:51 +0300

    class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+/*eslint-disable*/
+import React from 'react';
 import './App.css';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
