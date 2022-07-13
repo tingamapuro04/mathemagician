ce5dce8140318c0f35f4a0862c703a73455e230c
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-13 18:43:55 +0300

    complete testing

---
+++ src/snap.test.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Quote from './components/Quote';
+import Calculator from './components/calculator';
+import About from './components/About';
+import Nav from './components/nav'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Quote />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders 1', () => {
+  const tree = renderer
+    .create(<Calculator />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders the about', () => {
+  const tree = renderer
+    .create(<About />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
